fd0da51c4f88e7199834f4179c83d36ddb286379
HuangGuangBiao <chinaguangbiao@163.com>
2023-02-24 13:58:24 +0800

    Initial commit

---
+++ utils/src/main/java/com/location/utils/StringUtils.java
@@ -0,0 +1,12 @@
+package com.location.utils;
+
+/**
+ * @Author : HGuangBiao
+ * @Time : On 2023/2/24 13:49
+ * @Description : StringUtils
+ */
+public class StringUtils {
+    public static boolean isEmpty(String str){
+        return str==null||str.length()==0;
+    }
+}
